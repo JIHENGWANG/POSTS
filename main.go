a51174116e507f8ddd806a4a09a2d9cf00c2941a
jihengwang <jiw367@ucsd.edu>
2018-02-15 19:34:59 -0800

    App.yaml

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.67.171:9200"
+	ES_URL = "http://35.231.124.18:9200"
 
 )
 
